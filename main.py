fcfad6259996e61d32fd3a015c4eb666a54fd250
Divya <divyagudipati.g@gmail.com>
2024-04-02 13:22:02 +0530

    add faculty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from requests import request
 
@@ -8,15 +8,15 @@ conn=sqlite3.connect("Faculty")
 cursor=conn.cursor()
 try:
     conn.execute('BEGIN')
-    cursor.execute('drop table Faculty')
-    cursor.execute("create table Faculty ('Name' vachar2(20), 'Department' varchar2(30) , 'Fid' number(10), 'Mobile' number(10), 'Email' varchar2(50))")
-    cursor.execute("insert into Faculty Values ('Lakshmi','Maths',1214008,123567891,'lakshmi@gmail.com')")
-    cursor.execute("insert into Faculty values('Vijaya','Stats',1214010,1231231231,'vijaya@gmail.com')")
-    cursor.execute("insert into Faculty Values ('Rani','Computers',1214009,123567891,'rani@gmail.com')")
-    cursor.execute("insert into Faculty Values ('Priya','English',1214016,1234561234,'priya@gmail.com')")
-    cursor.execute("insert into Faculty Values ('Keerthi','Maths',1214001,1212121212,'keerthi@gmail.com')")
-    cursor.execute("insert into Faculty Values ('Rakesh','Physics',1214003,1234512345,'rakesh@gmail.com')")
-    cursor.execute("insert into Faculty Values ('Aishu','Chemistry',1214005,1234123412,'aishu@gmail.com')")
+    # cursor.execute('drop table Faculty')
+    # cursor.execute("create table Faculty ('Name' vachar2(20), 'Department' varchar2(30) , 'Fid' number(10), 'Mobile' number(10), 'Email' varchar2(50))")
+    # cursor.execute("insert into Faculty Values ('Lakshmi','Maths',1214008,123567891,'lakshmi@gmail.com')")
+    # cursor.execute("insert into Faculty values('Vijaya','Stats',1214010,1231231231,'vijaya@gmail.com')")
+    # cursor.execute("insert into Faculty Values ('Rani','Computers',1214009,123567891,'rani@gmail.com')")
+    # cursor.execute("insert into Faculty Values ('Priya','English',1214016,1234561234,'priya@gmail.com')")
+    # cursor.execute("insert into Faculty Values ('Keerthi','Maths',1214001,1212121212,'keerthi@gmail.com')")
+    # cursor.execute("insert into Faculty Values ('Rakesh','Physics',1214003,1234512345,'rakesh@gmail.com')")
+    # cursor.execute("insert into Faculty Values ('Aishu','Chemistry',1214005,1234123412,'aishu@gmail.com')")
     conn.commit()
 except Exception as e:
     print(e)
@@ -42,5 +42,21 @@ def Faculty(request:Request):
     finally:
         cursor.close()
         conn.close()
-
     return templates.TemplateResponse("index.html", {"request": request,"data": Faculty})
+@app.post('/add-faculty')
+def add_faculty(Name:str=Form(...),Department:str=Form(...),Fid:str=Form(...),Mobile:str=Form(...),Email:str=Form(...)):
+    conn=sqlite3.connect("Faculty")
+    cursor=conn.cursor()
+    try:
+        query=f"insert into Faculty (Name,Department,Fid,Mobile,Email) values('{Name}','{Department}','{Fid}','{Mobile}','{Email}')"
+        print(query)
+        conn.execute('BEGIN')
+        cursor.execute(query)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+        return True
